57c076b78a5d4d8ccf3c1f46ab0378b30ee0bf63
JaisonJohnPC <jaison.johns@gmail.com>
2021-11-14 22:13:36 +1100

    Converted to WAR file and inserted Bootstrap and Jquery

--- src/main/java/com/buyersAgent/BuyersAgentMainApplication.java
+++ src/main/java/com/buyersAgent/BuyersAgentMainApplication.java
@@ -8,11 +8,12 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@SecurityScheme(name = "buyersAgent", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition(info = @Info(title = "Buyers Agent API", version = "2.0", description = "Buyers Agent Information"))
-public class BuyersAgentMainApplication {
+public class BuyersAgentMainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuyersAgentMainApplication.class, args);
